e8e4a696e81e0e7146bad090380797c24b0beb12
Srikant Pattnaik <tosrikant@outlook.com>
2022-03-02 19:20:09 +0530

    first Commit

---
+++ src/test/java/com/sritest/myapp/cucumber/CucumberIT.java
@@ -0,0 +1,6 @@
+package com.sritest.myapp.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+class CucumberIT {}
---
+++ src/test/java/com/sritest/myapp/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.sritest.myapp.cucumber;
+
+import com.sritest.myapp.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
